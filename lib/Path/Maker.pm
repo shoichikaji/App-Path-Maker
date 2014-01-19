02644f9faf12cd2f7715279470330b3a9e95b561
Shoichi Kaji <skaji@outlook.com>
2014-01-19 22:17:29 +0900

    Checking in changes prior to tagging of version 0.003.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 71c28c2..a1f1910 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for Perl extension Path-Maker
    
     {{$NEXT}}
    
    +0.003 2014-01-19T13:16:55Z
    +
         - skip chmod tests if MSWin32
    
     0.002 2014-01-19T07:50:35Z

--- lib/Path/Maker.pm
+++ lib/Path/Maker.pm
@@ -8,7 +8,7 @@ use File::Path qw(mkpath);
 use File::Basename qw(dirname);
 use File::Spec::Functions qw(catdir catfile file_name_is_absolute);
 
-our $VERSION = "0.002";
+our $VERSION = "0.003";
 
 {
     package
